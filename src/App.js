0bce8e76e68f865ad60e0826a344c3ea1de3058b
kazouki <kazouki@hotmail.com>
2020-09-17 21:53:38 +0200

    add material-ui and, add basic material list component and, and item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import List from "./components/List";
+import AddItem from "./components/AddItem";
 
 function App() {
   return (
     <div className="App">
+      <AddItem />
+      <hr />
       <List listNr={1} />
     </div>
   );
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,8 +1,35 @@
 import React, { useState, useEffect } from "react";
 import api from "../../api";
 
+import { makeStyles } from "@material-ui/core/styles";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
 export default (props) => {
   const [state, setState] = useState({});
+  const classes = useStyles();
+  const [done, setDone] = useState(false);
+
+  const handleClick = () => {
+    setDone(!done);
+  };
 
   const fetchList = async () => {
     try {
@@ -23,5 +50,38 @@ export default (props) => {
       })
     : null;
 
-  return <div>{JSON.stringify(listMap)} </div>;
+  return (
+    <div>
+      {JSON.stringify(listMap)}
+
+      <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            todo items
+          </ListSubheader>
+        }
+        className={classes.root}
+      >
+        <ListItem button>
+          <ListItemIcon>
+            <SendIcon />
+          </ListItemIcon>
+          <ListItemText primary="send a mail" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="write a mail" />
+        </ListItem>
+        <ListItem button onClick={handleClick}>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+        </ListItem>
+      </List>
+    </div>
+  );
 };
